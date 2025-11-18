00654fd882175fb118fa9799134991673eb815b0
Tetsuro Mizuno <tt6-mzn@users.noreply.github.com>
2025-11-18 12:15:09 +0900

    typical90

--- typical90/src/bin/064.rs
+++ typical90/src/bin/064.rs
@@ -1,3 +1,29 @@
+use proconio::{input, marker::Usize1};
+
 fn main() {
-    todo!();
+    input! {
+        n: usize,
+        q: usize,
+        a: [i64; n],
+    };
+
+    let mut diff: Vec<i64> = a.windows(2).map(|w| { w[0] - w[1] }).collect();
+    let mut cost: i64 = diff.iter().map(|e| { e.abs() }).sum();
+
+    for _ in 0..q {
+        input! {
+            l: Usize1,
+            r: Usize1,
+            v: i64,
+        };
+        cost -= diff[l - 1].abs();
+        diff[l - 1] -= v;
+        cost += diff[l - 1].abs();
+        if r < n - 1 {
+            cost -= diff[r].abs();
+            diff[r] += v;
+            cost += diff[r].abs();
+        }
+        println!("{}", cost);
+    }
 }
--- typical90/src/bin/069.rs
+++ typical90/src/bin/069.rs
@@ -1,3 +1,32 @@
+use proconio::input;
+use ac_library::ModInt1000000007 as Mint;
+
 fn main() {
-    todo!();
+    input! {
+        (n, k): (u64, u32),
+    };
+
+    let mut ans = Mint::new(1);
+    if n == 1 {
+        println!("{}", k);
+        return;
+    }
+    if n == 2 && k == 1 {
+        println!("0");
+        return;
+    }
+    if n == 2 && k == 2 {
+        ans *= k;
+        ans *= k - 1;
+        println!("{}", ans.val());
+        return;
+    }
+    if n >= 3 && k <= 2 {
+        println!("0");
+        return;
+    }
+    ans *= k;
+    ans *= k - 1;
+    ans *= Mint::new(k - 2).pow(n - 2);
+    println!("{}", ans.val());
 }
