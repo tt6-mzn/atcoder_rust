825ccfdaef8301fffa01517e1e4c0df7cabcf961
Tetsuro Mizuno <tt6-mzn@users.noreply.github.com>
2025-11-19 22:45:25 +0900

    feat: implement solution for problem 052 calculating the product of sums of dice faces modulo 10^9+7

--- typical90/src/bin/052.rs
+++ typical90/src/bin/052.rs
@@ -1,3 +1,21 @@
+use ac_library::ModInt1000000007 as Mint;
+use proconio::input;
+
 fn main() {
-    todo!();
+    input! {
+        n: usize,
+        dices: [[u32; 6]; n],
+    };
+
+    let mut ans = Mint::new(1);
+    for i in 0..n {
+        ans = {
+            let mut d = Mint::new(0);
+            for j in 0..6 {
+                d += ans * dices[i][j];
+            };
+            d
+        };
+    }
+    println!("{}", ans);
 }
