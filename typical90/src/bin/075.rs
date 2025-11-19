5d5ee4353cb40b2cd9bfb27f496df722e439176d
Tetsuro Mizuno <tt6-mzn@users.noreply.github.com>
2025-11-19 21:07:48 +0900

    typical 90 075

--- typical90/src/bin/075.rs
+++ typical90/src/bin/075.rs
@@ -1,3 +1,28 @@
+use proconio::input;
+
 fn main() {
-    todo!();
+    input! {
+        mut n: usize,
+    };
+
+    let mut c = {
+        let mut ret: usize = 0;
+        let mut tn = n;
+        for i in 2.. {
+            if i * i > n {
+                break;
+            }
+            while tn % i == 0 {
+                tn /= i;
+                ret += 1;
+            }
+        }
+        if tn > 1 {
+            ret += 1;
+        }
+        ret
+    };
+
+    let ans = if c == 1 { 0 } else { (c - 1).ilog2() + 1 };
+    println!("{}", ans);
 }
