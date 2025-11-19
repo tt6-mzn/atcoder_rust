d0b4963d278e1813ac333bcfcc85ab63db95a05f
Tetsuro Mizuno <tt6-mzn@users.noreply.github.com>
2025-11-19 23:23:50 +0900

    feat: Implement dynamic programming solution for problem 050 using modular arithmetic.

--- typical90/src/bin/050.rs
+++ typical90/src/bin/050.rs
@@ -1,3 +1,21 @@
+use ac_library::ModInt1000000007 as Mint;
+use proconio::input;
+
 fn main() {
-    todo!();
+    input! {
+        n: usize,
+        l: usize,
+    }
+
+    // dp[i] := i段目から出発し、n段目に到達する方法の数
+    let mut dp = vec![Mint::new(0); n + 1];
+    dp[0] = Mint::new(1);
+    for i in 0..n {
+        let dpi = dp[i];
+        dp[i + 1] += dpi;
+        if i + l <= n {
+            dp[i + l] += dpi;
+        }
+    }
+    println!("{}", dp[n].val());
 }
